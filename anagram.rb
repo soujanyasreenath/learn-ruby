ae7ab774e51c2c0b95bf8b63c61d3b3c63312fd7
soujanyasreenath <soujanya.sreenath@gmail.com>
2016-06-15 12:43:34 +0530

    Anagram WIP

--- anagram.rb
+++ anagram.rb
@@ -1,37 +1,55 @@
-# puts "Enter the first word ... "
-# str1 = gets.chomp.downcase
+def anagram_input
+  puts "Enter the first word ... "
+  str1 = gets.chomp.downcase
 
-# puts "Enter the second word ... "
-# str2 = gets.chomp.downcase
+  puts "Enter the second word ... "
+  str2 = gets.chomp.downcase
 
-# chars1 = str1.chars
-# chars2 = str2.chars
+  chars1 = str1.chars
+  chars2 = str2.chars
 
-# puts "#{chars1}, #{chars2}"
+  puts "#{chars1}, #{chars2}"
 
-# if str1.length == str2.length
-#   if chars1.sort == chars2.sort
-#     puts "#{chars1.sort}, #{chars2.sort} - The strings are anagram"
-#   else
-#     puts "The strings are not anagram"
-#   end
-# else
-#   puts "sorry, the length of the 2 strings does not match!"
-# end
+  if str1.length == str2.length
+    if chars1.sort == chars2.sort
+      puts "#{chars1.sort}, #{chars2.sort} - The strings are anagram"
+    else
+      puts "The strings are not anagram"
+    end
+  else
+    puts "sorry, the length of the 2 strings does not match!"
+  end
 
-# anagram_array = ["tap", "spot", "mat", "pots", "tops", "pat", "pit", "tam", "tip", "melt"]
+  anagram_array = ["tap", "spot", "mat", "pots", "tops", "pat", "pit", "tam", "tip", "melt"]
 
-# anagram_array.each do |p|
-#   # p_split = p.split("")
-#   p_sort = anagram_array.sort
-#   puts p_sort
+  anagram_array.each do |p|
+    # p_split = p.split("")
+    p_sort = anagram_array.sort
+    puts p_sort
 
-# end
-
-anagram_array = ["tap", "stop", "mat", "stop", "stop", "tap", "pit", "mat", "pit", "melt"]
-
-anagram_array.each do |p|
-  p_ch = p.chars
-  p_sr = p_ch.sort
-  puts p_sr.join
+  end
 end
+
+def dup_array
+  duplicate_array = ["tap", "stop", "mat", "stop", "stop", "tap", "pit", "mat", "pit", "melt"]
+
+  duplicate_array.each do |p|
+    p_ch = p.chars
+    p_sr = p_ch.sort
+    puts p_sr.join
+  end
+end
+
+def ana_array
+  anagram_array = ["tap", "stop", "mat", "tops", "pots", "pat", "pit", "tam", "tip", "melt"]
+
+  anagram_array.each do |p|
+    p_ch = p.chars
+    p_sr = p_ch.sort
+    puts p_sr.join
+  end
+end
+  
+anagram_input
+dup_array
+ana_array
