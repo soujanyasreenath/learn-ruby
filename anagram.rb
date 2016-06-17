a8930b55158f667ffc5108765d98a9ad6dbdf47f
soujanyasreenath <soujanya.sreenath@gmail.com>
2016-06-17 13:57:40 +0530

    anagram array - WIP

--- anagram.rb
+++ anagram.rb
@@ -31,25 +31,31 @@ def anagram_input
 end
 
 def dup_array
-  duplicate_array = ["tap", "stop", "mat", "stop", "stop", "tap", "pit", "mat", "pit", "melt"]
+  duplicate_array = ["tap", "stop", "mat", "stop", "melt", "malt", "stop", "tap", "pit", "mat", "pit", "melt"]
+  sorted_duplicate_array = duplicate_array.sort
+  puts "finding duplicate in #{duplicate_array}"
+  i = 0
+  max = sorted_duplicate_array.length 
 
   duplicate_array.each do |p|
-    p_ch = p.chars
-    p_sr = p_ch.sort
-    puts p_sr.join
+    for i in i..max
+      if sorted_duplicate_array[i] == sorted_duplicate_array[i-1]
+        puts "duplicate #{sorted_duplicate_array[i]} = #{sorted_duplicate_array[i-1]}"
+      end   
+    end
   end
 end
 
 def ana_array
   anagram_array = ["tap", "stop", "mat", "tops", "pots", "pat", "pit", "tam", "tip", "melt"]
-
+  i = 0
+  max = anagram_array.length 
   anagram_array.each do |p|
-    p_ch = p.chars
-    p_sr = p_ch.sort
-    puts p_sr.join
+    p_join = p.chars.sort.join
+    print p_join
   end
 end
   
-anagram_input
-dup_array
+# anagram_input
+# dup_array
 ana_array
